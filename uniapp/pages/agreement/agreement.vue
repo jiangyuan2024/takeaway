75c9963b97128af401a77ac8fe56de91933e5140
username <useremail@qq.com>
2023-12-29 16:44:28 +0800

    初始化项目版本~

---
+++ uniapp/pages/agreement/agreement.vue
@@ -0,0 +1,55 @@
+<template>
+	<view style="padding: 20rpx;">
+		<view class="box">
+			<view style="font-size: 40rpx; margin-bottom: 20rpx;">用户协议</view>
+			<view class="contain">
+				<pre>
+					<text style="font-weight: bold;padding:20rpx 0;display:inline-block;">一、概述</text><br />
+					欢迎您使用本外卖APP。<br />
+					本用户协议（以下简称“本协议”）是您（以下简称“用户”）与外卖APP（以下简称“本APP”）之间就用户使用本APP提供的服务所订立的协议。<br />
+					在使用本APP的服务之前，请您务必仔细阅读本协议的所有内容。如果您不同意本协议的任何内容，您应立即停止使用本APP的服务。<br />
+					
+					<text style="font-weight: bold;padding:20rpx 0;display:inline-block;">二、服务内容</text><br />
+					1. 本APP提供的服务包括但不限于外卖订购、配送服务等。<br />
+					2. 用户使用本APP的服务时，应遵守本APP的相关规定，并承担因自己的行为产生的一切责任。<br />
+					
+					<text style="font-weight: bold;padding:20rpx 0;display:inline-block;">三、用户权利和义务</text><br />
+					1. 用户有权按照本APP的规定使用服务。<br />
+					2. 用户在使用本APP的服务时，应遵守国家相关法律法规，不得利用本APP的服务从事违法活动。<br />
+					3. 用户应妥善保管自己的账号和密码，不得将其提供给他人使用。<br />
+					4. 用户不得利用本APP的服务从事任何可能对本APP或他人造成损害的行为。<br />
+					
+					<text style="font-weight: bold;padding:20rpx 0;display:inline-block;">四、隐私保护</text><br />
+					本APP将严格保护用户的个人隐私信息，不会将其提供给任何第三方机构或个人。用户在使用本APP的服务时，应遵守本APP的隐私保护政策。<br />
+					
+					<text style="font-weight: bold;padding:20rpx 0;display:inline-block;">五、免责声明</text><br />
+					1. 本APP不对用户使用本APP的服务造成的任何损失承担责任，包括但不限于因网络原因、不可抗力等造成的损失。<br />
+					2. 用户使用本APP的服务时，应自行承担风险，本APP不对用户因使用本APP的服务而产生的任何损失承担责任。<br />
+					
+					<text style="font-weight: bold;padding:20rpx 0;display:inline-block;">六、其他</text><br />
+					1. 本协议的解释权归本APP所有。<br />
+					2. 本APP有权在必要时修改本协议的内容，用户应定期关注本协议的变动。如用户不同意本协议的变动，应立即停止使用本APP的服务。<br />
+					
+					<text style="padding-top:30rpx;display:inline-block;">以上就是外卖APP的用户协议的内容，用户在使用本APP的服务时应遵守以上规定。</text>
+				</pre>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
